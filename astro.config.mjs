15da679a64a9e6f5ee607ad779d120e295ffe105
Steven Fewster <steven.fewster@gmail.com>
2026-06-15 12:37:53 +0100

    Moving tailwind for v6 Astro

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,11 @@
 import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
-import tailwind from "@astrojs/tailwind";
 import vue from "@astrojs/vue";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://stevenfewster.com",
   output: "static",
-  integrations: [mdx(), sitemap(), tailwind(), vue()],
+  integrations: [mdx(), sitemap(), vue()],
 });
